25b12557f885ef07799e4d8c9c40e065f03838d7
chuongduong <duongthuychuong@gmail.com>
2022-11-21 07:45:44 +0700

    tour review project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,58 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  const removeTours = (id) => {
+    return setTours(tours.filter((tour) => tour.id !== id));
+  };
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setTours(tours);
+      setLoading(false);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTours();
+  }, []);
+  if (loading) {
+    return (
+      <main>
+        {" "}
+        <Loading />
+      </main>
+    );
+  }
+
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>There is no tour</h2>
+          <button className="btn" onClick={fetchTours}>
+            Fetch tours
+          </button>
+        </div>
+      </main>
+    );
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTours={removeTours} />
+    </main>
+  );
 }
 
-export default App
+export default App;
--- src/Tour.js
+++ src/Tour.js
@@ -1,7 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
-const Tour = () => {
-  return <h2>tour component</h2>;
+const Tour = ({ id, image, info, price, name, removeTours }) => {
+  const [readMore, setReadmore] = useState(false);
+  return (
+    <article className="single-tour">
+      <img src={image} alt="" />
+      <footer>
+        <div className="tour-info">
+          <h4>{name}</h4>
+          <h4 className="tour-price">${price}</h4>
+        </div>
+      </footer>
+      <p>
+        {readMore ? info : `${info.substring(0, 200)}...`}{" "}
+        <button onClick={() => setReadmore(!readMore)}>
+          {readMore ? "Read less" : "Read more"}
+        </button>
+      </p>
+
+      <button
+        onClick={() => {
+          removeTours(id);
+        }}
+        className="delete-btn"
+      >
+        Not Interested
+      </button>
+    </article>
+  );
 };
 
 export default Tour;
